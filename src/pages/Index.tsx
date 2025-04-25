22b242e9749bc391292500a244f3c2462246381b
kosarshojaei <kosarshojaei16@gmail.com>
2025-04-25 13:17:09 +0330

    replace the site

---
+++ src/pages/Index.tsx
@@ -0,0 +1,29 @@
+
+import { useEffect } from "react";
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Projects from "@/components/Projects";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
+
+const Index = () => {
+  const profileImage = "/lovable-uploads/f2a11f14-d856-4d92-b31c-86e024c84e4a.png";
+  
+  useEffect(() => {
+    document.title = "Kosar Shojaei | Portfolio";
+  }, []);
+
+  return (
+    <div className="min-h-screen bg-white">
+      <Navbar />
+      <Hero profileImage={profileImage} />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
